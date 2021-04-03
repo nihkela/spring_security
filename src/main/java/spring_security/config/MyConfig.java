9967e6e120f997ed84092d1f8cc80f32961d54a9
Alexey Alekhin <nihkela@gmail.com>
2021-04-03 17:20:25 +0300

    Create Spring Security config

---
+++ src/main/java/spring_security/config/MyConfig.java
@@ -0,0 +1,25 @@
+package spring_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "spring_security")
+@EnableWebMvc
+public class MyConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+        internalResourceViewResolver.setSuffix(".jsp");
+
+        return internalResourceViewResolver;
+    }
+
+
+}
---
+++ src/main/java/spring_security/config/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package spring_security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
